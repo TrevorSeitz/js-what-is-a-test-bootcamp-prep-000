63e9dc30745a66b29e8c3968311ecf3ae4e43d31
Trevor Seitz <trevinla@gmail.com>
2018-02-16 17:50:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ var height = 74
 var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
+
+console.log(message)
+
 <<<<<<< HEAD
 .
 =======
 .
 >>>>>>> ad69964e7a5cb1d7096f41fb55c1e2af621a805f
-
-console.log(message)
